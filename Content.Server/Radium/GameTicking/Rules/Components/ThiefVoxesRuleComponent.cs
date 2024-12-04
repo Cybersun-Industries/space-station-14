cc6f7333d2a79a894b26bca3c9d7575fc191ffe5
Ilysha998 <sukhachew.ilya@gmail.com>
2024-12-04 19:28:28 +0300

    Vox Raiders
    я НЕ ПОНИМАЮ как выдать им антага и цели.

---
+++ Content.Server/Radium/GameTicking/Rules/Components/ThiefVoxesRuleComponent.cs
@@ -0,0 +1,11 @@
+using Content.Shared.Random;
+using Robust.Shared.Audio;
+using Robust.Shared.Prototypes;
+
+namespace Content.Server.GameTicking.Rules.Components;
+
+/// <summary>
+/// Stores data for <see cref="ThiefVoxesRuleSystem"/>.
+/// </summary>
+[RegisterComponent, Access(typeof(ThiefVoxesRuleSystem))]
+public sealed partial class ThiefVoxesRuleComponent : Component;
