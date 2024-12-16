0a91d77516c44b9c4abe6018f0fdd81cd1dffc91
AwareFoxy <135021509+AwareFoxy@users.noreply.github.com>
2024-12-16 03:12:57 +0100

    Merge pull request #166 from Vonsant/StockTrading
    Биржевая торговля

--- Content.Server/Cargo/Systems/CargoSystem.Orders.cs
+++ Content.Server/Cargo/Systems/CargoSystem.Orders.cs
@@ -99,8 +99,8 @@ namespace Content.Server.Cargo.Systems
             {
                 _timer -= Delay;
 
-                var stationQuery = EntityQueryEnumerator<StationBankAccountComponent>();
-                while (stationQuery.MoveNext(out var uid, out var bank))
+                var stationQuery = EntityQueryEnumerator<StationBankAccountComponent>(); // Corvax-Next-StockTrading: Early merge #33123
+                while (stationQuery.MoveNext(out var uid, out var bank)) // Corvax-Next-StockTrading: Early merge #33123
                 {
                     var balanceToAdd = bank.IncreasePerSecond * Delay;
                     UpdateBankAccount((uid, bank), balanceToAdd);
@@ -229,7 +229,7 @@ namespace Content.Server.Cargo.Systems
                 $"{ToPrettyString(player):user} approved order [orderId:{order.OrderId}, quantity:{order.OrderQuantity}, product:{order.ProductId}, requester:{order.Requester}, reason:{order.Reason}] with balance at {bank.Balance}");
 
             orderDatabase.Orders.Remove(order);
-            UpdateBankAccount((station.Value, bank), -cost);
+            UpdateBankAccount((station.Value, bank), -cost); // Corvax-Next-StockTrading: Early merge #33123
             UpdateOrders(station.Value);
         }
 
