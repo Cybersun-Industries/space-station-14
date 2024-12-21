394cbb7913c0a2beda88d97bb5d44402a35d8596
freeze2222 <opop1094@gmail.com>
2024-12-21 22:16:15 +0300

    Fix tests ( it already took too long... )

--- Content.IntegrationTests/Tests/Toolshed/AdminTest.cs
+++ Content.IntegrationTests/Tests/Toolshed/AdminTest.cs
@@ -1,7 +1,6 @@
 ﻿using System.Collections.Generic;
-using System.Linq;
 using Robust.Shared.Toolshed;
-using Robust.UnitTesting.Shared.Toolshed;
+
 
 namespace Content.IntegrationTests.Tests.Toolshed;
 
