388b2bac75d7aa8d080a308884445cb2520d4eef
Ilysha998 <sukhachew.ilya@gmail.com>
2024-12-29 12:30:54 +0300

    Ивентинг некстген
    Ура?

---
+++ Content.Server/Radium/TimedMapLightChanging/Components/MapTimeChangingComponent.cs
@@ -0,0 +1,39 @@
+namespace Content.Server.Radium.TimedMapLightChanging.Components
+{
+    [RegisterComponent]
+    public sealed partial class TimedMapLightChangingComponent : Component
+    {
+        [ViewVariables(VVAccess.ReadWrite)]
+        [DataField]
+        public Color MorningColor = Color.FromHex("#0232DD");
+
+        [ViewVariables(VVAccess.ReadWrite)]
+        [DataField]
+        public Color DayColor = Color.FromHex("#666666");
+
+        [ViewVariables(VVAccess.ReadWrite)]
+        [DataField]
+        public Color EveningColor = Color.FromHex("#FAD6A5");
+
+        [ViewVariables(VVAccess.ReadWrite)]
+        [DataField]
+        public Color NightColor = Color.FromHex("#000000");
+
+        [ViewVariables(VVAccess.ReadWrite)]
+        [DataField]
+        public float MorningDuration = 600;
+
+        [ViewVariables(VVAccess.ReadWrite)]
+        [DataField]
+        public float DayDuration = 1200;
+
+        [ViewVariables(VVAccess.ReadWrite)]
+        [DataField]
+        public float EveningDuration = 600;
+
+        [ViewVariables(VVAccess.ReadWrite)]
+        [DataField]
+        public float NightDuration = 1200;
+    }
+}
+
---
+++ Content.Server/Radium/TimedMapLightChanging/MapTimeChangingSystem.cs
@@ -0,0 +1,87 @@
+using Content.Server.Radium.TimedMapLightChanging.Components;
+using Robust.Shared.Map.Components;
+using Robust.Shared.Timing;
+
+namespace Content.Server.Radium.TimedMapLightChanging;
+
+/// <summary>
+/// This handles timeticking
+/// </summary>
+public sealed class TimedMapLightChangingSystem : EntitySystem
+{
+    [Dependency] private readonly IGameTiming _gameTiming = default!;
+
+    public override void Update(float frameTime)
+    {
+        base.Update(frameTime);
+
+        var query = EntityQueryEnumerator<TimedMapLightChangingComponent, MapLightComponent>();
+        while (query.MoveNext(out var uid, out var timedMapLight, out var mapLight))
+        {
+
+            var morningDuration = timedMapLight.MorningDuration;
+            var dayDuration = timedMapLight.DayDuration;
+            var eveningDuration = timedMapLight.EveningDuration;
+            var nightDuration = timedMapLight.NightDuration;
+            var transitionDuration = dayDuration / 2f;
+            var daycycleDuration = morningDuration + dayDuration + eveningDuration + nightDuration;
+
+            var t = (float)_gameTiming.CurTime.TotalSeconds % daycycleDuration / daycycleDuration;
+
+            if (t <= 0.25f) // Утро
+            {
+                var morningColor = timedMapLight.DayColor;
+                if (t >= 0.25f - (transitionDuration / morningDuration))
+                {
+                    var transitionT = (0.25f - t) / (transitionDuration / morningDuration);
+                    morningColor = Color.InterpolateBetween(timedMapLight.NightColor,
+                        timedMapLight.MorningColor,
+                        transitionT);
+                }
+
+                mapLight.AmbientLightColor = morningColor;
+            }
+            else if (t <= 0.5f) // День
+            {
+                var dayColor = timedMapLight.DayColor;
+                if (t >= 0.5f - (transitionDuration / dayDuration))
+                {
+                    var transitionT = (0.5f - t) / (transitionDuration / dayDuration);
+                    dayColor = Color.InterpolateBetween(timedMapLight.NightColor,
+                        timedMapLight.DayColor,
+                        transitionT);
+                }
+
+                mapLight.AmbientLightColor = dayColor;
+            }
+            else if (t <= 0.75f) // Вечер
+            {
+                var eveningColor = timedMapLight.EveningColor;
+                if (t <= 0.5f + (transitionDuration / eveningDuration))
+                {
+                    var transitionT = (t - 0.5f) / (transitionDuration / eveningDuration);
+                    eveningColor = Color.InterpolateBetween(timedMapLight.DayColor,
+                        timedMapLight.EveningColor,
+                        transitionT);
+                }
+
+                mapLight.AmbientLightColor = eveningColor;
+            }
+            else // Ночь
+            {
+                var nightColor = timedMapLight.NightColor;
+                if (t <= 1f - (transitionDuration / nightDuration))
+                {
+                    var transitionT = (t - 0.75f) / (transitionDuration / nightDuration);
+                    nightColor = Color.InterpolateBetween(timedMapLight.EveningColor,
+                        timedMapLight.NightColor,
+                        transitionT);
+                }
+
+                mapLight.AmbientLightColor = nightColor;
+            }
+
+            Dirty(uid, mapLight);
+        }
+    }
+}
