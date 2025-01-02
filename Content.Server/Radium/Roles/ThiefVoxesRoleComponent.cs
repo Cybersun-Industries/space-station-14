f06505a3b12e96610a02a07b7227c45227c3cec9
Ilysha998 <sukhachew.ilya@gmail.com>
2025-01-02 15:29:15 +0300

    e

---
+++ Content.Server/Radium/Roles/ThiefVoxesRoleComponent.cs
@@ -0,0 +1,11 @@
+using Content.Shared.Roles;
+
+namespace Content.Server.Radium.Roles;
+
+/// <summary>
+///     Added to mind role entities to tag that they are a vox thieves.
+/// </summary>
+[RegisterComponent]
+public sealed partial class ThiefVoxesRoleComponent : BaseMindRoleComponent
+{
+}
