b116bb59d73b2c4d102241a5d526914ad800dec2
CybersunBot <cybersunbot@proton.me>
2025-06-07 17:11:17 +0000

    chore: Automatically update REUSE headers

--- Content.Radium.Common/AssemblyInfo.cs
+++ Content.Radium.Common/AssemblyInfo.cs
@@ -1,3 +1,4 @@
+// SPDX-FileCopyrightText: 2025 CybersunBot <cybersunbot@proton.me>
 // SPDX-FileCopyrightText: 2025 freeze2222 <opop1094@gmail.com>
 //
 // SPDX-License-Identifier: AGPL-3.0-or-later
--- Content.Radium.Server/Stars/EntitySystems/EnergyEmitterSystem.cs
+++ Content.Radium.Server/Stars/EntitySystems/EnergyEmitterSystem.cs
@@ -21,6 +21,7 @@
 // SPDX-FileCopyrightText: 2024 Piras314 <p1r4s@proton.me>
 // SPDX-FileCopyrightText: 2024 metalgearsloth <31366439+metalgearsloth@users.noreply.github.com>
 // SPDX-FileCopyrightText: 2025 Aiden <28298836+Aidenkrz@users.noreply.github.com>
+// SPDX-FileCopyrightText: 2025 CybersunBot <cybersunbot@proton.me>
 // SPDX-FileCopyrightText: 2025 freeze2222 <opop1094@gmail.com>
 // SPDX-FileCopyrightText: 2025 slarticodefast <161409025+slarticodefast@users.noreply.github.com>
 //
