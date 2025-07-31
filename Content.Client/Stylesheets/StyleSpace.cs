2763223a5cd4f35fa8fa9b55b85eb505b2c9d603
CybersunBot <cybersunbot@proton.me>
2025-07-31 19:52:22 +0000

    chore: Automatically update REUSE headers

--- Content.Client/Stylesheets/StyleSpace.cs
+++ Content.Client/Stylesheets/StyleSpace.cs
@@ -10,6 +10,9 @@
 // SPDX-FileCopyrightText: 2023 metalgearsloth <31366439+metalgearsloth@users.noreply.github.com>
 // SPDX-FileCopyrightText: 2025 Aiden <28298836+Aidenkrz@users.noreply.github.com>
 // SPDX-FileCopyrightText: 2025 CybersunBot <cybersunbot@proton.me>
+// SPDX-FileCopyrightText: 2025 Ilysha998 <sukhachew.ilya@gmail.com>
+// SPDX-FileCopyrightText: 2025 SX-7 <sn1.test.preria.2002@gmail.com>
+// SPDX-FileCopyrightText: 2025 freeze2222 <46649391+freeze2222@users.noreply.github.com>
 // SPDX-FileCopyrightText: 2025 freeze2222 <opop1094@gmail.com>
 //
 // SPDX-License-Identifier: MIT
