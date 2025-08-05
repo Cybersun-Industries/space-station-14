6e1bf66fb3c36e64a672509190270a4a7820a3a0
CybersunBot <cybersunbot@proton.me>
2025-08-05 08:41:11 +0000

    chore: Automatically update REUSE headers

--- Content.Server/Corvax/StationGoal/StationGoalPaperComponent.cs
+++ Content.Server/Corvax/StationGoal/StationGoalPaperComponent.cs
@@ -1,3 +1,8 @@
+// SPDX-FileCopyrightText: 2025 CybersunBot <cybersunbot@proton.me>
+// SPDX-FileCopyrightText: 2025 Ilysha998 <sukhachew.ilya@gmail.com>
+//
+// SPDX-License-Identifier: AGPL-3.0-or-later
+
 namespace Content.Server.Corvax.StationGoal
 {
     /// <summary>
