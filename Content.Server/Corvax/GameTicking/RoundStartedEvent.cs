80c5e89e7752264a9d50b3772a51ad907e68dadb
Ilysha998 <sukhachew.ilya@gmail.com>
2025-08-05 14:45:15 +0700

    whitespace fix

--- Content.Server/Corvax/GameTicking/RoundStartedEvent.cs
+++ Content.Server/Corvax/GameTicking/RoundStartedEvent.cs
@@ -3,7 +3,7 @@ namespace Content.Shared.GameTicking;
 public sealed class RoundStartedEvent : EntityEventArgs
 {
     public int RoundId { get; }
-    
+
     public RoundStartedEvent(int roundId)
     {
         RoundId = roundId;
