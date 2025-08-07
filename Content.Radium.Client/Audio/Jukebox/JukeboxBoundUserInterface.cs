37e3b8378cf2343137dd19c071ed3fcf4e2ee9c8
CybersunBot <cybersunbot@proton.me>
2025-08-07 00:00:06 +0000

    chore: Automatically update REUSE headers

--- Content.Radium.Client/Audio/Jukebox/JukeboxBoundUserInterface.cs
+++ Content.Radium.Client/Audio/Jukebox/JukeboxBoundUserInterface.cs
@@ -3,6 +3,7 @@
 // SPDX-FileCopyrightText: 2024 iNVERTED <alextjorgensen@gmail.com>
 // SPDX-FileCopyrightText: 2024 metalgearsloth <31366439+metalgearsloth@users.noreply.github.com>
 // SPDX-FileCopyrightText: 2025 Aiden <28298836+Aidenkrz@users.noreply.github.com>
+// SPDX-FileCopyrightText: 2025 XO6bl4 <49454110+XO6bl4@users.noreply.github.com>
 //
 // SPDX-License-Identifier: AGPL-3.0-or-later
 
--- Content.Radium.Client/Audio/Jukebox/JukeboxSystem.cs
+++ Content.Radium.Client/Audio/Jukebox/JukeboxSystem.cs
@@ -4,6 +4,7 @@
 // SPDX-FileCopyrightText: 2025 CybersunBot <cybersunbot@proton.me>
 // SPDX-FileCopyrightText: 2025 SX-7 <sn1.test.preria.2002@gmail.com>
 // SPDX-FileCopyrightText: 2025 Tayrtahn <tayrtahn@gmail.com>
+// SPDX-FileCopyrightText: 2025 XO6bl4 <49454110+XO6bl4@users.noreply.github.com>
 //
 // SPDX-License-Identifier: AGPL-3.0-or-later
 
