191c58eeb07cb0be3e9d6483916b4802ba75915a
CybersunBot <cybersunbot@proton.me>
2025-08-13 19:16:47 +0000

    chore: Automatically update REUSE headers

--- Content.Radium.Server/AddComponentOnUse/AddComponentsOnUseComponent.cs
+++ Content.Radium.Server/AddComponentOnUse/AddComponentsOnUseComponent.cs
@@ -1,3 +1,7 @@
+// SPDX-FileCopyrightText: 2025 MorDast1 <morgit1488@gmail.com>
+//
+// SPDX-License-Identifier: AGPL-3.0-or-later
+
 using Robust.Shared.Prototypes;
 
 namespace Content.Server.ADT.AddComponentsOnUse;
--- Content.Radium.Server/AddComponentOnUse/AddComponentsOnUseSystem.cs
+++ Content.Radium.Server/AddComponentOnUse/AddComponentsOnUseSystem.cs
@@ -1,3 +1,7 @@
+// SPDX-FileCopyrightText: 2025 MorDast1 <morgit1488@gmail.com>
+//
+// SPDX-License-Identifier: AGPL-3.0-or-later
+
 using Content.Shared.Interaction.Events;
 
 namespace Content.Server.Radium.AddComponentsOnUse;
